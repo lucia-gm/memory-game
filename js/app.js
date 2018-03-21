7a520ca7a60c9aa11c94a948f7740d1676d1f498
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-03-20 20:41:45 -0700

    Refactoring matching conditions and preventing double click the same card

--- js/app.js
+++ js/app.js
@@ -1,73 +1,4 @@
 /*
- * Create a list that holds all of your cards
- */
-const cards = document.getElementsByClassName('card');
-var openCards=[];
-
-function noMatch() {
-  if (openCards.length === 2 && !(openCards[0].isEqualNode(openCards[1]))) {
-    for (let i=0; i<openCards.length; i++) {
-      openCards[i].classList.remove('open', 'show');
-    }
-    openCards=[];
-  }
-}
-
-//Cheking if the cards match
-function match() {
-  if (openCards.length === 2 && openCards[0].isEqualNode(openCards[1])) {
-    for (let i=0; i<openCards.length; i++) {
-      openCards[i].classList.add('match');
-      openCards[i].classList.remove('open', 'show');
-    }
-    openCards=[];
-  } else {
-    noMatch();
-  }
-  // } else {
-  //   for (let i=0; i<openCards.length; i++) {
-  //     openCards[i].classList.remove('open', 'show');
-  //   }
-  // }
-}
-
-
-
-
-// Listening if a card is clicked
-for (let i=0; i<cards.length; i++) {
-  cards[i].addEventListener('click', function flipCard() {
-    cards[i].classList.add('show', 'open');
-    openCards.push(this);
-    match();
-  });
-}
-
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-
-/*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
@@ -77,3 +8,66 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+// Variables
+const cards = document.getElementsByClassName('card');
+var cardsOpen = [];
+
+
+
+// Check if cards match
+function match() {
+  if (cardsOpen.length === 2) {
+    if (cardsOpen[0].isEqualNode(cardsOpen[1])) {
+      for (let i = 0; i < cardsOpen.length; i++) {
+        cardsOpen[i].classList.add('match');
+        cardsOpen[i].classList.remove('open', 'show');
+      }
+      cardsOpen = [];
+    } else {
+      noMatch();
+    }
+  }
+}
+
+// If cards don't match, error state
+function noMatch() {
+  // @TODO add timer and error state
+
+  for (let i = 0; i < cardsOpen.length; i++) {
+    cardsOpen[i].classList.remove('open', 'show');
+  }
+  cardsOpen = [];
+}
+
+// Shuffle cards so it doesn´t get repetitive. Function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+
+
+// Listening if a card is clicked
+for (let i = 0; i < cards.length; i++) {
+  cards[i].addEventListener('click', function flipCard() {
+
+    if (cards[i] !== cardsOpen[0]) { // Checking if it is the same card
+      cards[i].classList.add('open', 'show');
+      cardsOpen.push(this);
+
+      match();
+    }
+  });
+}
