5366b2e69cd06850bf3008395d5d9aa665d9391a
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-03-26 17:56:34 -0700

    fix: Modal doesn't display if winning for 2nd time

--- js/app.js
+++ js/app.js
@@ -170,6 +170,7 @@ function resetGame() {
   moves = 0;
   movesCounter.innerText = 0;
   sec = 0;
+  matchings = 0;
   seconds.innerText = '00';
   minutes.innerText = '00';
   modal.style.display = "none";
